777ee2df3471c3fb32dce601dddf3a3b679ddcfb
yogitasharma18 <93505365+yogitasharma18@users.noreply.github.com>
2022-04-05 01:10:16 +0530

    Add files via upload

---
+++ StudentManagementSystem/src/main/java/com/greatLearning/studentmanagement/service/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.greatLearning.studentmanagement.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.greatLearning.studentmanagement.entity.User;
+import com.greatLearning.studentmanagement.repository.UserRepository;
+import com.greatLearning.studentmanagement.security.MyUserDetails;
+
+
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	   @Autowired
+	    private UserRepository userRepository;
+	     
+	    @Override
+	    public UserDetails loadUserByUsername(String username)
+	            throws UsernameNotFoundException {
+	        User user = userRepository.getUserByUsername(username);
+	         
+	        if (user == null) {
+	            throw new UsernameNotFoundException("Could not find user");
+	        }
+	         
+	        return new MyUserDetails(user);
+	    }
+
+}
